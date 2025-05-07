df16d797e7387e9e89547aa6892a2326a14108dd
Matias Almaraz <malmaraz@frba.utn.edu.ar>
2025-05-07 20:27:57 -0300

    registro y cambio de rol

--- src/main/java/Registro.java
+++ src/main/java/Registro.java
@@ -1,2 +1,25 @@
 public class Registro {
+  private String nombre;
+  private String apellido;
+  private int edad;
+
+  public Registro(String nombre) {
+    if (nombre == null || nombre.trim().isEmpty()) {
+      throw new IllegalArgumentException("El nombre es requerido");
+    }
+    this.nombre = nombre;
+    this.edad = 0;
+    this.apellido = "";
+  }
+
+  public Registro(String nombre, String apellido, int edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+
+  }
+
 }
+
+
+
--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,8 +1,22 @@
 public class Usuario extends Persona {
   public Rol rol = new Visualizador();
+  private Registro registro;
+
+  public Usuario(Rol rol, Registro registro) {
+    this.rol = rol;
+    this.registro = registro;
+  }
 
   @Override
   public void subirHecho() {
     this.rol = rol.subirHecho();
+  }
+
+  public void cambiarRol(Rol rol) {
+    this.rol = rol;
+  }
+
+  public void registrarse(Registro registro) {
+    this.registro = registro;
   }
 }
