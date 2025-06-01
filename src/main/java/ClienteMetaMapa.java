cf3a1ac6c45dd647dbcf46298ad5dc83404c08e1
Franco Tenaglia <ftenaglia@frba.utn.edu.ar>
2025-06-01 11:22:28 -0300

    Agrego mapper al cliente

--- src/main/java/ClienteMetaMapa.java
+++ src/main/java/ClienteMetaMapa.java
@@ -1,15 +1,33 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 public class ClienteMetaMapa {
-  public List<Hecho> getHechos() throws Exception{
-    HttpClient client = HttpClient.newHttpClient();
 
+  private static final String url = "http://localhost:8080/apiMetaMapa";
+
+  private final HttpClient client;
+  private final ObjectMapper mapper;
+
+  public ClienteMetaMapa() {
+    this.client = HttpClient.newHttpClient();
+    this.mapper = new ObjectMapper();
+  }
+
+  public List<Hecho> getHechos() throws Exception{
     HttpRequest request = HttpRequest.newBuilder()
-        .uri(new URI("/hechos"))
+        .uri(new URI(url + "/hechos"))
         .GET()
         .build();
+
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    if (response.statusCode() == 200) {
+      return mapper.readValue(response.body(), new TypeReference<List<Hecho>>() {});
+    } else {
+      throw new RuntimeException("Error en la petición: " + response.statusCode());
+    }
   }
 }
