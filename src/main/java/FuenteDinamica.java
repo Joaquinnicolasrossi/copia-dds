945e5062f7e4437f2fe432ca080ee2687e7a60fa
Matias Almaraz <malmaraz@frba.utn.edu.ar>
2025-06-01 16:50:07 -0300

    actualizar hecho

--- src/main/java/FuenteDinamica.java
+++ src/main/java/FuenteDinamica.java
@@ -1,10 +1,25 @@
 public class FuenteDinamica {
   RepoFuenteDinamica repoFuenteDinamica;
 
-
-  public void subirHecho(Hecho hecho){
+  public void subirHecho(Hecho hecho) {
     repoFuenteDinamica.save(hecho);
   }
 
+  public void actualizarHecho(String titulo, Hecho.HechoBuilder hechoBuilder) throws Exception {
+    var hecho = getHecho(titulo);
 
+    if (!hecho.estaDentroDePlazoDeEdicion()) {
+      throw new Exception("El plazo para modificar este hecho ha expirado.");
+    }
+
+    Hecho hechoActualizado = repoFuenteDinamica.saveUpdate(hecho, hechoBuilder);
+    if (hechoActualizado == null) {
+      throw new Exception("No pudo actualizar el hecho");
+    }
+  }
+
+  private Hecho getHecho(String titulo) {
+    Hecho hecho = repoFuenteDinamica.findByTitulo(titulo);
+    return hecho;
+  }
 }
--- src/main/java/RepoFuenteDinamica.java
+++ src/main/java/RepoFuenteDinamica.java
@@ -14,6 +14,11 @@ public class RepoFuenteDinamica {
         .findFirst()
         .orElse(null);
   }
-
+  public Hecho saveUpdate(Hecho hechoOriginal, Hecho.HechoBuilder hechoBuilder) {
+    Hecho actualizado = hechoOriginal.actualizarHechoConBuilderParcial(hechoOriginal, hechoBuilder);
+    hechos.removeIf(h -> h.getTitulo().equals(hechoOriginal.getTitulo()));
+    hechos.add(actualizado);
+    return actualizado;
+  }
 
 }
