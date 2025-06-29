9627e21584995b35901a44f3caec967392ff0333
Franco Tenaglia <ftenaglia@frba.utn.edu.ar>
2025-06-29 12:48:47 -0300

    Fuente agregada concurrente

--- src/main/java/FuenteAgregada.java
+++ src/main/java/FuenteAgregada.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class FuenteAgregada implements Fuente {
   private final List<Fuente> fuentes;
@@ -10,11 +14,31 @@ public class FuenteAgregada implements Fuente {
 
   @Override
   public List<Hecho> extraerHechos() {
-    List<Hecho> hechos = new ArrayList<>();
-    hechos = fuentes.stream()
-        .flatMap(fuente -> fuente.extraerHechos().stream())
-        .toList();;
-    return filtrarRepetidos(hechos);
+
+    try (ExecutorService executor = Executors.newFixedThreadPool(fuentes.size())) {
+      List<Callable<List<Hecho>>> tareas = fuentes.stream()
+          .<Callable<List<Hecho>>>map(fuente -> fuente::extraerHechos)
+          .toList();
+
+      List<Future<List<Hecho>>> resultados = executor.invokeAll(tareas);
+
+      List<Hecho> hechos = resultados.stream()
+          .map(futuro -> {
+            try {
+              return futuro.get();
+            } catch (Exception e) {
+              return List.<Hecho>of();
+            }
+          })
+          .flatMap(List::stream)
+          .toList();
+
+      return filtrarRepetidos(hechos);
+
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new RuntimeException("Consulta interrumpida", e);
+    }
   }
 
   private List<Hecho> filtrarRepetidos(List<Hecho> hechos) {
